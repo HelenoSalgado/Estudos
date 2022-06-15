c3ff2d4dc3fde1c5bce20b3b362a51d58a087595
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2022-06-15 19:11:32 -0300

    :up:

--- exercicios-javascript/botao-de-curtir/js/like.js
+++ exercicios-javascript/botao-de-curtir/js/like.js
@@ -7,7 +7,7 @@ if(localStorage.curtidas == undefined){
 }
 
 async function getLikes(){
-    let response = await fetch("https://heleno.epizy.com/php/get-likes.php");
+    let response = await fetch("botao-de-curtir.atwebpages.com/php/get-likes.php");
     var data = await response.json();
     mostra.innerText = data.curtidas;
 }getLikes();
@@ -30,7 +30,7 @@ curtidas.addEventListener("click", () =>{
 
 async function postLike(){
 
-fetch("https://heleno.epizy.com/php/increment-likes.php", {
+fetch("botao-de-curtir.atwebpages/php/increment-likes.php", {
     method: "POST",
     mode: "no-cors",
     headers: {
